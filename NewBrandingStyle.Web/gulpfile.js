644800836a2b3f6d1ce7bf072b18aa33121acfe9
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2019-10-22 21:00:47 +0200

    configure SCSS compilation via gulp

---
+++ NewBrandingStyle.Web/gulpfile.js
@@ -0,0 +1,14 @@
+﻿/// <binding AfterBuild='scss' />
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+const paths = {
+    scss: "./Styles/**/*.scss",
+    css: "./wwwroot/css/"
+};
+
+gulp.task("scss", () => {
+    return gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.css));
+});
