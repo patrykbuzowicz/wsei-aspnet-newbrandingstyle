4de360d812fbfabfb2deab7d76fdad90f1e533a5
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2019-10-22 18:23:50 +0200

    add processing user form data and displaying results

--- NewBrandingStyle.Web/Controllers/CompanyController.cs
+++ NewBrandingStyle.Web/Controllers/CompanyController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Models;
 
 namespace NewBrandingStyle.Web.Controllers
 {
     public class CompanyController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+
+            return View("CompanyAdded", viewModel);
         }
     }
 }
