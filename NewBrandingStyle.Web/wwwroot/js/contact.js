e94b40907c806cc412714b6592c5ad05d8d29291
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2019-11-13 19:06:10 +0100

    contact form: check response.ok in AJAX response

--- NewBrandingStyle.Web/wwwroot/js/contact.js
+++ NewBrandingStyle.Web/wwwroot/js/contact.js
@@ -2,21 +2,28 @@
     var form = document.getElementById("contact-form");
 
     form.addEventListener("submit", event => {
+        // make sure the browser doesn't submit the form as usual
         event.preventDefault();
 
+        // read the form.elements fields
         var formData = {
             email: form.elements.Email.value,
             message: form.elements.Message.value
         };
 
+        // process the collected data
         fetch("/contact/submit", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(formData)
-        }).then(() => {
-            showAlert("alert-success");
+        }).then(response => {
+            if (response.ok) {
+                showAlert("alert-success");
+            } else {
+                showAlert("alert-error");
+            }
         }).catch(() => {
             showAlert("alert-error");
         });
