787e6b758569298a997987edcdb2cdef67377633
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2019-11-29 23:48:35 +0100

    CompanyEntity + Company/List.cshtml

---
+++ NewBrandingStyle.Web/Entities/CompanyEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace NewBrandingStyle.Web.Entities
+{
+    public class CompanyEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
